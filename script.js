e0b456d388154975f9f20cc8f0185ea68c11a2d2
Lalit Mehra <lokeshmehra7039@gmail.com>
2025-03-27 17:28:48 +0530

    add hamburger and close functionality

--- script.js
+++ script.js
@@ -108,5 +108,15 @@ async function main() {
         currentSong.currentTime = (currentSong.duration*percent)/100
     })
 
+    // Add an event listner for hamburger 
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left ="0"
+    })
+
+    // Add an event listner for close button 
+    document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left = "-120%"
+    })
+
 }
 main()
